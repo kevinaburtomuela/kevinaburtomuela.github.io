deb065e56711f7be47383fce2eedec90519beeab
KevinAburto <kevinaburtomuela@hotmail.com>
2026-06-23 18:20:44 -0600

    Fix: text description

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -15,14 +15,16 @@ themeBtn.addEventListener("click", () => {
 const translations = {
   en: {
     about_me: "ABOUT ME",
-    about_me_text: "I am a tester with more than 8 years of experience in the testing area. I have an ISTQB certification and a Scrum certification. I have 7 years of experience working with Agile methodologies, 6 years of experience using SQL Server, and 2 years of experience using Oracle. I also have 6 years of experience using Jira, as well as various applications for test case creation, bug reporting, and trazability tools",
+    about_me_text: "I am a tester with 9 years of experience in the testing area. I have an ISTQB certification and a Scrum certification. I have 7 years of experience working with Agile methodologies, 6 years of experience using SQL Server, and 2 years of experience using Oracle. I also have 6 years of experience using Jira, as well as various applications for test case creation, bug reporting, and trazability tools",
     work_experience: "WORK EXPERIENCE",
-    fecha_qualtop: "January 2024 – May 2025 | CDMX",
+    fecha_qualtop: "March 2024 – March 2026 | CDMX",
     qualtop_perfoned: "Performed functional manual testing (black box, UAT, regression, confirmation, smoke, and system testing).",
     qualtop_analyzed: "Analyzed client requirements using Jira to create test cases.",
     qualtop_api: "Performed API testing using Postman.",
     qualtop_created: "Created and executed SQL Server queries.",
     qualtop_managed: "Managed test lifecycle using Jira and XRAY.",
+    qualtop_cypress: "Developed automated test cases using Cypress.",
+    qualtop_docker: "Configured and executed automated test suites in Jenkins and managed code deployment using Docker.",
     date_ready:"June 2022 – December 2023", 
     ready_review: "Reviewed requirements using Figma and Jira.",
     ready_perfomed: "Performed UI testing based on Figma files.",
@@ -70,20 +72,22 @@ const translations = {
     other_courses: "Other courses",
     projects: "PROJECTS",
     contact: "CONTACT",
-    project1: "QA Playground Bank Demo is a simulated banking application designed for practicing end-to-end test automation.",
-    project2: "Automated end-to-end testing of the OrangeHRM web application, covering authentication, employee management, leave management, recruitment workflows, and dashboard validation.",
-    project3: "A web application designed for QA automation practice, featuring a variety of common web elements and testing scenarios such as login, forms, dynamic content, file uploads, and JavaScript interactions."
+    project1: "QA Playground Bank Demo is a simulated banking application used for end-to-end test automation. The project's objective was to validate critical functionalities such as user authentication, savings account creation, and banking transactions through automated testing. A framework based on industry best practices was implemented to improve script maintainability and scalability, reducing manual effort in regression testing while enabling early defect detection. This project strengthened my skills in test automation, framework design, test case management, version control, and CI/CD integration.",
+    project2: "OrangeHRM is an open-source Human Resource Management (HRM) application used to practice end-to-end test automation in a real-world business environment. The project's objective was to validate critical HR workflows, including user authentication, employee management, recruitment, leave requests, and personnel administration through automated testing. A scalable automation framework was implemented following industry best practices to ensure maintainability, reusability, and efficient test execution. As a result, regression testing efforts were reduced, application reliability was improved, and defects were identified earlier in the development cycle. This project enhanced my skills in test automation, framework design, test case development, version control, and CI/CD integration while providing hands-on experience with complex business processes and enterprise-level web applications.",
+    project3: "The Internet Herokuapp is a web application designed for practicing functional testing and test automation across a variety of common web application scenarios. The project's objective was to validate critical functionalities such as authentication, dynamic elements, file uploads, alerts, tables, forms, and navigation through automated testing. An automation framework was developed following industry best practices to ensure maintainability, reusability, and scalability of the test scripts. As a result, regression testing became more efficient, functional test coverage was increased, and defects were identified earlier in the development cycle. This project strengthened my skills in test automation, framework design, test case development, web element identification, synchronization handling, and software quality best practices."
   },
   es: {
     about_me: "SOBRE MÍ",
-    about_me_text: "Soy un tester con más de 8 años de experiencia en el área de pruebas. Cuento con una certificación ISTQB y una certificación Scrum. Tengo 7 años de experiencia trabajando con metodologías ágiles, 6 años de experiencia utilizando SQL Server y 2 años de experiencia utilizando Oracle. También tengo 6 años de experiencia usando Jira, así como diferentes aplicaciones para la creación de casos de prueba, reporte de errores y herramientas de trazabilidad.",
+    about_me_text: "Soy un tester con 9 años de experiencia en el área de pruebas. Cuento con una certificación ISTQB y una certificación Scrum. Tengo 7 años de experiencia trabajando con metodologías ágiles, 6 años de experiencia utilizando SQL Server y 2 años de experiencia utilizando Oracle. También tengo 6 años de experiencia usando Jira, así como diferentes aplicaciones para la creación de casos de prueba, reporte de errores y herramientas de trazabilidad.",
     work_experience: "EXPERIENCIA LABORAL",
-    fecha_qualtop: "Enero 2024 – Mayo 2025 | CDMX",    
+    fecha_qualtop: "Marzo 2024 – Marzo 2026 | CDMX",    
     qualtop_perfoned: "Realicé pruebas funcionales manuales (caja negra, UAT, regresión, confirmación, smoke y pruebas de sistema).",
     qualtop_analyzed: "Analicé los requerimientos del cliente utilizando Jira para crear casos de prueba.",
     qualtop_api: "Realicé pruebas de APIs usando Postman.",    
     qualtop_created: "Crear y ejecutar consultas en SQL Server.",    
-    qualtop_managed: "Gestioné el ciclo de vida de las pruebas utilizando Jira y Xray.",  
+    qualtop_managed: "Gestioné el ciclo de vida de las pruebas utilizando Jira y Xray.", 
+    qualtop_cypress: "Generé casos de prueba en Cypress.",
+    qualtop_docker: "Ejecute y configure la automatización del set de pruebas en Jenkins y el despliegue del código en Docker.", 
     date_ready:"Junio 2022 – Diciembre 2023",  
     ready_review: "Analicé los requerimientos utilizando Figma y Jira.",    
     ready_perfomed: "Realicé pruebas de interfaz de usuario (UI) basadas en archivos de Figma.",    
@@ -130,9 +134,9 @@ const translations = {
     other_courses: "Otros cursos",
     projects: "PROYECTOS",
     contact: "CONTACTO",
-    project1: "QA Playground Bank Demo es una aplicación bancaria simulada diseñada para practicar pruebas de automatización end-to-end.",
-    project2: "Pruebas automatizadas end-to-end de la aplicación web OrangeHRM, cubriendo autenticación, gestión de empleados, gestión de permisos y ausencias, flujos de reclutamiento y validación del panel de control (dashboard).",
-    project3: "Una aplicación web diseñada para la práctica de automatización de pruebas QA, que incluye una variedad de elementos web y escenarios de prueba comunes, como inicio de sesión, formularios, contenido dinámico, carga de archivos e interacciones con JavaScript."
+    project1: "QA Playground Bank Demo es una aplicación bancaria simulada utilizada para la automatización de pruebas end-to-end. El proyecto tuvo como objetivo validar funcionalidades críticas como autenticación de usuarios, creación de cuentas de ahorro y operaciones bancarias mediante pruebas automatizadas. Se implementó una estructura basada en buenas prácticas para mejorar la mantenibilidad y escalabilidad de los scripts, logrando reducir el esfuerzo manual en pruebas de regresión y detectar defectos de forma temprana. Este proyecto fortaleció mis habilidades en automatización de pruebas, diseño de frameworks, gestión de casos de prueba e integración con herramientas de control de versiones y CI/CD",
+    project2: "OrangeHRM es una aplicación de código abierto para la gestión de recursos humanos (HRM) utilizada para practicar la automatización de pruebas end-to-end en un entorno empresarial real. El objetivo del proyecto fue validar flujos críticos de recursos humanos, incluyendo autenticación de usuarios, gestión de empleados, reclutamiento, solicitudes de permisos y administración de personal mediante pruebas automatizadas. Se implementó un framework de automatización escalable siguiendo buenas prácticas de la industria para garantizar la mantenibilidad, reutilización y ejecución eficiente de las pruebas. Como resultado, se redujo el esfuerzo requerido para las pruebas de regresión, se mejoró la confiabilidad de la aplicación y se identificaron defectos de manera temprana durante el ciclo de desarrollo. Este proyecto fortaleció mis habilidades en automatización de pruebas, diseño de frameworks, desarrollo de casos de prueba, control de versiones e integración con CI/CD, además de brindarme experiencia práctica en procesos de negocio complejos y aplicaciones web de nivel empresarial.",
+    project3: "The Internet Herokuapp es una aplicación web diseñada para la práctica de pruebas funcionales y automatización en diversos escenarios comunes de aplicaciones web. El objetivo del proyecto fue validar funcionalidades críticas como autenticación, manejo de elementos dinámicos, cargas de archivos, alertas, tablas, formularios y navegación mediante pruebas automatizadas. Se desarrolló un framework de automatización siguiendo buenas prácticas para garantizar la mantenibilidad, reutilización y escalabilidad de los scripts de prueba. Como resultado, se logró una ejecución más eficiente de las pruebas de regresión, una mayor cobertura de escenarios funcionales y una detección temprana de defectos. Este proyecto fortaleció mis habilidades en automatización de pruebas, diseño de frameworks, creación de casos de prueba, localización de elementos web, manejo de sincronización y aplicación de buenas prácticas de calidad de software."
   }
 };
 
